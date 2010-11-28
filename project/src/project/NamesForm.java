26bf82e0340824023f05176ec2eb6eb0c4dd5c71
Dominik Schöler <dominik.schoeler@makandra.de>
2010-11-28 16:49:42 +0200

    created/refined GUI elements: game, history, names

--- project/src/project/NamesForm.java
+++ project/src/project/NamesForm.java
@@ -1,4 +1,11 @@
 package project;
 
+import javax.swing.*;
+
 public class NamesForm {
+    private JPanel panel1;
+    private JTextField pleaseEnterTheNameTextField;
+    private JTextField pleaseEnterTheNameTextField1;
+    private JButton startNewGameButton;
+    private JButton cancelButton;
 }
